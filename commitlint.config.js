251b97380ce4f6f9becd9f719a32dd28d6f1ea98
Andres <andres.correa@designli.co>
2026-05-24 12:20:27 -0500

    build: enforce commit header < 100 chars

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 99], // C11: < 100 chars
+    'body-max-line-length': [2, 'always', 100],
+  },
 }
